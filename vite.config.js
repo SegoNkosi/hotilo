6c76848c2abf0e48d7f702637c22449a9b45de66
Nkosi Lesego <nkosilesego8@gmail.com>
2025-10-05 03:13:21 +0200

    debugging

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss()
   ],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 5173
+  }
 })
